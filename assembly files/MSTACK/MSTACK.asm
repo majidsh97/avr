f04b9862537e229083d800ab8bab8d7533afd401
majidsh97 <sharghi.majid@gmail.com>
2018-01-24 18:10:15 +0330

    Add files via upload

---
+++ assembly files/MSTACK/MSTACK.asm
@@ -0,0 +1,9 @@
+#ifndef _MSTACK_ASM_
+#define _MSTACK_ASM_
+#include "MSTACK/MSTACK.INC"
+
+
+
+
+
+#endif	
---
+++ assembly files/resource/RES.asm
@@ -0,0 +1,6 @@
+#ifndef _RES_ASM_
+#define _RES_ASM_
+#include "RESOURCE\RES.INC"
+
+
+#endif	
